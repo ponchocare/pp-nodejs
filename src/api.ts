fc5aa1ac3c448296dcf3ff57d19878d64f6d2e53
Carlos Ortega <carlosV2.0@gmail.com>
2025-04-03 18:20:14 +0100

    build a low-lever API object

---
+++ src/api.ts
@@ -0,0 +1,47 @@
+import fetch, { type HeadersInit, type Response } from 'node-fetch';
+import { joinPaths, serialise, telemetry } from './utils';
+
+export class Api {
+  private base: string;
+
+  public constructor(base: string = 'https://pay.ponchopay.com/') {
+    this.base = base;
+  }
+
+  private getUrl(path: string): string {
+    return joinPaths(this.base, path);
+  }
+
+  private getHeaders(): HeadersInit {
+    return {
+      'content-type': 'application/json',
+      'x-telemetry': serialise(telemetry()),
+    };
+  }
+
+  public async makePostRequest(
+    path: string,
+    headers: HeadersInit,
+    body: string,
+  ): Promise<Response> {
+    return await fetch(this.getUrl(path), {
+      method: 'post',
+      headers: { ...headers, ...this.getHeaders() },
+      body,
+      redirect: 'manual',
+    });
+  }
+
+  public async makePutRequest(
+    url: string,
+    headers: HeadersInit,
+    body: string,
+  ): Promise<Response> {
+    return await fetch(this.getUrl(url), {
+      method: 'put',
+      headers: { ...headers, ...this.getHeaders() },
+      body,
+      redirect: 'manual',
+    });
+  }
+}
---
+++ test/api.spec.ts
@@ -0,0 +1,45 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+
+const fetchMock = vi.hoisted(() => vi.fn());
+vi.mock('node-fetch', () => ({ default: fetchMock }));
+
+import { Api } from '../src/api';
+import { serialise } from '../src/utils';
+
+const base = 'pay.ponchopay.com';
+const demo = 'demo.ponchopay.com';
+const path = '/endpoint/path';
+const headers = { 'Accept-Language': 'en' };
+const body = serialise({ payment: '9207f21a', amount: 123 });
+
+describe.each([
+  { param: undefined, expected: base, description: 'default base' },
+  { param: `https://${demo}/`, expected: demo, description: 'demo url' },
+])('Api (With $description)', async ({ param, expected }) => {
+  beforeEach(() => fetchMock.mockClear());
+
+  describe.each([
+    { method: 'post', fn: 'makePostRequest' },
+    { method: 'put', fn: 'makePutRequest' },
+  ])('$fn', ({ method, fn }) => {
+    it('sends a request', async () => {
+      const mockedResponse = new Response('response', { status: 200 });
+      fetchMock.mockResolvedValueOnce(mockedResponse);
+
+      const api = new Api(param);
+      const expectedResponse = await api[fn](path, headers, body);
+
+      expect(expectedResponse).toBe(mockedResponse);
+      expect(fetchMock).toHaveBeenCalledWith(`https://${expected}${path}`, {
+        method,
+        headers: {
+          'Accept-Language': 'en',
+          'content-type': 'application/json',
+          'x-telemetry': expect.any(String),
+        },
+        body,
+        redirect: 'manual',
+      });
+    });
+  });
+});
