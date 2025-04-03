1b09699a2a27a84e4909e805859607f5fd93e615
Carlos Ortega <carlosV2.0@gmail.com>
2025-04-03 18:20:45 +0100

    update Client with the new methods

--- src/client.ts
+++ src/client.ts
@@ -1,50 +1,103 @@
-import fetch from 'node-fetch';
+import { createJWT, createToken } from './token';
+import { PPError } from './error';
+import { Api } from './api';
+import { replaceParams, Serialisable, serialise } from './utils';
 
-import { createToken } from './token.js';
-import { PPError } from './error.js';
+type ISO8601Date = `${number}-${number}-${number}`;
+type ISO8601Time = `${number}:${number}:${number}.${number}Z`;
+type DateValue = `${ISO8601Date}T${ISO8601Time}` | Date;
 
-type PaymentInit = {
-  metadata: string;
-  urn: string;
+type Constraints = { minimum_card_amount?: number };
+
+type BasePaymentInit = {
   amount: number;
-  email: string;
+  metadata: string;
   note?: string;
-  expiry?: Date | string;
+  expiry?: DateValue;
+  constraints?: Constraints;
 };
 
-const INITIATE_PAYMENT_ENDPOINT = '/api/integration/generic/initiate';
+type PaymentInit = BasePaymentInit & {
+  urn: string;
+  email: string;
+};
 
+export enum Weekday {
+  Monday = 'monday',
+  Tuesday = 'tuesday',
+  Wednesday = 'wednesday',
+  Thursday = 'thursday',
+  Friday = 'friday',
+  Saturday = 'saturday',
+  Sunday = 'sunday',
+}
+
+type SubscriptionRepetition =
+  | { granularity: 'daily'; period: number }
+  | { granularity: 'weekly'; period: number; weekdays: Set<Weekday> }
+  | { granularity: 'monthly'; period: number; day: number }
+  | { granularity: 'yearly'; period: number };
+
+type SubscriptionEnding =
+  | { condition: 'never' }
+  | { condition: 'occurrences'; occurrences: number }
+  | { condition: 'date'; date: DateValue };
+
+type SubscriptionInit = {
+  urn: string;
+  amount: number;
+  metadata: string;
+  note?: string;
+  email: string;
+  repetition: SubscriptionRepetition;
+  ending?: SubscriptionEnding;
+  additional_one_time_payment?: BasePaymentInit;
+};
+
+export enum PaymentMethodType {
+  Card = 'card',
+  ChildcareVouchers = 'childcare-voucher',
+  TaxFreeChildcare = 'tax-free-childcare',
+}
+
+type JWTPayload = {
+  urn: string;
+  email: string;
+};
+
+type PaymentMethodUpdate = JWTPayload & {
+  type: PaymentMethodType;
+  amount: number;
+  voucher_provider?: string;
+};
+
+type PaymentMethodRefund = JWTPayload & {
+  amount: number;
+};
+
+/**
+ * PonchoPay client.
+ * It collects the methods to manipulate payments in PonchoPay.
+ */
 export class Client {
-  private key: string;
-  private base: string;
+  private readonly api!: Api;
+  private readonly key: string;
 
   public constructor(key: string, base: string = 'https://pay.ponchopay.com/') {
+    this.api = new Api(base);
     this.key = key;
-
-    while (base.slice(-1) === '/') {
-      base = base.slice(0, -1);
-    }
-    this.base = base;
   }
 
-  private getUrl(endpoint: string): string {
-    return `${this.base}${endpoint}`;
-  }
-
-  private getData(init: PaymentInit): string {
-    return JSON.stringify({
-      ...init,
-      token: createToken(this.key, init.metadata),
+  private async getRedirectLocation(
+    url: string,
+    data: { metadata: string } & Serialisable,
+  ): Promise<string> {
+    const body = serialise({
+      ...data,
+      token: createToken(this.key, data.metadata),
     });
-  }
 
-  public async initiatePayment(init: PaymentInit): Promise<string> {
-    const response = await fetch(this.getUrl(INITIATE_PAYMENT_ENDPOINT), {
-      method: 'post',
-      headers: { 'content-type': 'application/json' },
-      body: this.getData(init),
-      redirect: 'manual',
-    });
+    const response = await this.api.makePostRequest(url, {}, body);
 
     if (response.status === 302) {
       const location = response.headers.get('location');
@@ -59,6 +112,88 @@ export class Client {
 
     throw new PPError(
       `Unexpected response. Expected 302 as status code but ${response.status} was received.`,
+    );
+  }
+
+  private async issuePutRequest(
+    url: string,
+    { urn, email, ...data }: JWTPayload & Serialisable,
+  ): Promise<void> {
+    const stringified = serialise(data);
+    const jwt = await createJWT(urn, this.key, email, stringified);
+
+    const headers = { Authorization: `Bearer ${jwt}` };
+    const response = await this.api.makePutRequest(url, headers, stringified);
+
+    if (response.status !== 204) {
+      throw new PPError(
+        `Unexpected response. Expected 204 as status code but ${response.status} was received.`,
+      );
+    }
+  }
+
+  /**
+   * Initiates a payment.
+   * This method returns a URL to redirect the user to.
+   */
+  public async initiatePayment(init: PaymentInit): Promise<string> {
+    return await this.getRedirectLocation(
+      '/api/integration/generic/initiate',
+      init,
+    );
+  }
+
+  /**
+   * Initiates a subscription.
+   * This method returns a URL to redirect the user to.
+   */
+  public async initiateSubscription(init: SubscriptionInit): Promise<string> {
+    return await this.getRedirectLocation(
+      '/api/integration/generic/subscription',
+      init,
+    );
+  }
+
+  /**
+   * Updates a single payment method within a payment.
+   */
+  public async updatePaymentMethod(
+    paymentMethodId: string,
+    update: PaymentMethodUpdate,
+  ): Promise<void> {
+    await this.issuePutRequest(
+      replaceParams('/api/payment-method/[paymentMethodId]', {
+        paymentMethodId,
+      }),
+      update,
+    );
+  }
+
+  /**
+   * Refunds a single payment method within a payment.
+   */
+  public async refundPaymentMethod(
+    paymentMethodId: string,
+    refund: PaymentMethodRefund,
+  ): Promise<void> {
+    await this.issuePutRequest(
+      replaceParams('/api/payment-method/[paymentMethodId]/refund', {
+        paymentMethodId,
+      }),
+      refund,
+    );
+  }
+
+  /**
+   * Requests the cancelation of payment.
+   */
+  public async cancelPayment(
+    paymentId: string,
+    payload: JWTPayload,
+  ): Promise<void> {
+    await this.issuePutRequest(
+      replaceParams('/api/payment/[paymentId]/cancel', { paymentId }),
+      payload,
     );
   }
 }
--- test/client.spec.ts
+++ test/client.spec.ts
@@ -1,125 +1,373 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest';
 
-const { fetchMock } = vi.hoisted(() => ({ fetchMock: vi.fn() }));
-vi.mock('node-fetch', () => ({ default: fetchMock }));
+const makePostRequestMock = vi.hoisted(() => vi.fn());
+const makePutRequestMock = vi.hoisted(() => vi.fn());
+const apiMock = vi.hoisted(() =>
+  vi.fn().mockReturnValue({
+    makePostRequest: makePostRequestMock,
+    makePutRequest: makePutRequestMock,
+  }),
+);
+vi.mock('../src/api', () => ({ Api: apiMock }));
 
-import { Client } from '../src';
+import { Client, Weekday, PaymentMethodType } from '../src/client';
+import { serialise } from '../src/utils';
 import { PPError } from '../src/error';
 
-const token = 'KxS2N+iLo3WCgBbPN8sAEUUaA2jNOLuVG/kkkqQrBI4=';
-const defaults = {
-  metadata: 'order-1234',
-  urn: 'URN1234',
-  amount: 1234,
-  email: 'some@guy.com',
-};
+const urn = 'IUpGPArQ';
+const email = 'help@ponchopay.com';
+const key = '6N28tFbrufnfCT58ZvmzIwaL8S1aVFryIasJazFqdc516T/1ZrLw7CDqOSlF5NeF';
+const base = 'https://some.base/url';
+const location = 'https://returned/location';
+const authorization = expect.stringMatching(/^Bearer\seyJhbGciOiJIUzI1NiJ9/);
 
-describe('Client', async () => {
-  const Response = (
-    await vi.importActual<typeof import('node-fetch')>('node-fetch')
-  ).Response;
+describe('Client', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
 
-  beforeEach(() => fetchMock.mockClear());
+  describe('constructor', () => {
+    it('ensures the api is created with the default base', async () => {
+      new Client(key);
+      expect(apiMock).toHaveBeenCalledWith('https://pay.ponchopay.com/');
+    });
+
+    it('ensures the api is created with the given base', async () => {
+      new Client(key, base);
+      expect(apiMock).toHaveBeenCalledWith(base);
+    });
+  });
 
   describe('initiatePayment', () => {
-    it('requests a url for a payment', async () => {
-      const url = 'http://some/url';
-      const client = new Client('key');
+    const token = 'ytfBNCiHCbU/WdEZ1yEB60DsMpgD7VgR0SSqgJhj0mY=';
+    const defaults = { metadata: 'order-1234', urn, amount: 1234, email };
 
-      fetchMock.mockResolvedValueOnce(Response.redirect(url, 302));
-      expect(await client.initiatePayment(defaults)).toBe(url);
-      expect(fetchMock).toHaveBeenCalledWith(
-        'https://pay.ponchopay.com/api/integration/generic/initiate',
-        {
-          body: JSON.stringify({ ...defaults, token }),
-          headers: { 'content-type': 'application/json' },
-          method: 'post',
-          redirect: 'manual',
-        },
+    it('initiates a new payment', async () => {
+      const response = Response.redirect(location, 302);
+      makePostRequestMock.mockResolvedValueOnce(response);
+
+      const client = new Client(key);
+
+      await expect(client.initiatePayment(defaults)).resolves.toBe(location);
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/initiate',
+        {},
+        serialise({ ...defaults, token }),
       );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
     });
 
-    it('requests a url for a payment on another server', async () => {
-      const url = 'http://some/url';
-      const client = new Client('key', 'https://other.server/');
+    it('initiates a payment with a note', async () => {
+      const response = Response.redirect(location, 302);
+      makePostRequestMock.mockResolvedValueOnce(response);
 
-      fetchMock.mockResolvedValueOnce(Response.redirect(url, 302));
-      expect(await client.initiatePayment(defaults)).toBe(url);
-      expect(fetchMock).toHaveBeenCalledWith(
-        'https://other.server/api/integration/generic/initiate',
-        {
-          body: JSON.stringify({ ...defaults, token }),
-          headers: { 'content-type': 'application/json' },
-          method: 'post',
-          redirect: 'manual',
-        },
-      );
-    });
+      const client = new Client(key);
 
-    it('requests a url for a payment with a note', async () => {
-      const url = 'http://some/url';
-      const init = {
+      const data = {
         ...defaults,
         note: "Here's your dough, now go have some fun-dough",
       };
-      const client = new Client('key');
-
-      fetchMock.mockResolvedValueOnce(Response.redirect(url, 302));
-      expect(await client.initiatePayment(init)).toBe(url);
-      expect(fetchMock).toHaveBeenCalledWith(
-        'https://pay.ponchopay.com/api/integration/generic/initiate',
-        {
-          body: JSON.stringify({ ...init, token }),
-          headers: { 'content-type': 'application/json' },
-          method: 'post',
-          redirect: 'manual',
-        },
+      await expect(client.initiatePayment(data)).resolves.toBe(location);
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/initiate',
+        {},
+        serialise({ ...data, token }),
       );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
     });
 
-    it.each([
-      [new Date(1705403600000), '2024-01-16T11:13:20.000Z'],
-      ['some string', 'some string'],
-    ])(
-      'requests a url for a payment with an expiry date',
-      async (sending, sent) => {
-        const url = 'http://some/url';
-        const init = {
-          ...defaults,
-          expiry: sending,
-        };
-        const client = new Client('key');
+    it('initiates a payment with an expiry date', async () => {
+      const response = Response.redirect(location, 302);
+      makePostRequestMock.mockResolvedValueOnce(response);
 
-        fetchMock.mockResolvedValueOnce(Response.redirect(url, 302));
-        expect(await client.initiatePayment(init)).toBe(url);
-        expect(fetchMock).toHaveBeenCalledWith(
-          'https://pay.ponchopay.com/api/integration/generic/initiate',
-          {
-            body: JSON.stringify({ ...init, expiry: sent, token }),
-            headers: { 'content-type': 'application/json' },
-            method: 'post',
-            redirect: 'manual',
-          },
-        );
-      },
-    );
+      const client = new Client(key);
+
+      const data = { ...defaults, expiry: '2025-04-03T12:57:16.000Z' } as const;
+      await expect(client.initiatePayment(data)).resolves.toBe(location);
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/initiate',
+        {},
+        serialise({ ...data, token }),
+      );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
+    });
+
+    it('initiates a payment with a minimum card amount constraint', async () => {
+      const response = Response.redirect(location, 302);
+      makePostRequestMock.mockResolvedValueOnce(response);
+
+      const client = new Client(key);
+
+      const data = { ...defaults, constraints: { minimum_card_amount: 123 } };
+      await expect(client.initiatePayment(data)).resolves.toBe(location);
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/initiate',
+        {},
+        serialise({ ...data, token }),
+      );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
+    });
 
     it('fails if the response from the server is not a redirect', async () => {
-      const client = new Client('key');
+      const response = new Response('all good?', { status: 200 });
+      makePostRequestMock.mockResolvedValueOnce(response);
 
-      fetchMock.mockResolvedValueOnce(
-        new Response('all good?', { status: 200 }),
+      const client = new Client(key);
+
+      await expect(client.initiatePayment(defaults)).rejects.toBeInstanceOf(
+        PPError,
       );
-      expect(client.initiatePayment(defaults)).rejects.toBeInstanceOf(PPError);
-      expect(fetchMock).toHaveBeenCalledWith(
-        'https://pay.ponchopay.com/api/integration/generic/initiate',
-        {
-          body: JSON.stringify({ ...defaults, token }),
-          headers: { 'content-type': 'application/json' },
-          method: 'post',
-          redirect: 'manual',
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/initiate',
+        {},
+        serialise({ ...defaults, token }),
+      );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('initiateSubscription', () => {
+    const token = 'Axii0jwfzrkfh57WttOCi3qi+icnUC2dFKtre3b4pM4=';
+    const defaults = {
+      urn,
+      amount: 1234,
+      metadata: 'subscription-1234',
+      email,
+      repetition: {
+        granularity: 'weekly',
+        period: 2,
+        weekdays: new Set([Weekday.Tuesday, Weekday.Friday]),
+      },
+    } as const;
+
+    it('initiates a new subscription', async () => {
+      const response = Response.redirect(location, 302);
+      makePostRequestMock.mockResolvedValueOnce(response);
+
+      const client = new Client(key);
+      const subscription = await client.initiateSubscription(defaults);
+
+      expect(subscription).toBe(location);
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/subscription',
+        {},
+        serialise({ ...defaults, token }),
+      );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
+    });
+
+    it('initiates a new subscription with a note', async () => {
+      const response = Response.redirect(location, 302);
+      makePostRequestMock.mockResolvedValueOnce(response);
+
+      const data = {
+        ...defaults,
+        note: "Here's your dough, now go have some fun-dough",
+      };
+
+      const client = new Client(key);
+      const subscription = await client.initiateSubscription(data);
+
+      expect(subscription).toBe(location);
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/subscription',
+        {},
+        serialise({ ...data, token }),
+      );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
+    });
+
+    it('initiates a new subscription with an ending', async () => {
+      const response = Response.redirect(location, 302);
+      makePostRequestMock.mockResolvedValueOnce(response);
+
+      const data = {
+        ...defaults,
+        ending: { condition: 'date', date: '2024-01-16T11:13:20.000Z' },
+      } as const;
+
+      const client = new Client(key);
+      const subscription = await client.initiateSubscription(data);
+
+      expect(subscription).toBe(location);
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/subscription',
+        {},
+        serialise({ ...data, token }),
+      );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
+    });
+
+    it('initiates a new subscription with an additional one time payment', async () => {
+      const response = Response.redirect(location, 302);
+      makePostRequestMock.mockResolvedValueOnce(response);
+
+      const data = {
+        ...defaults,
+        additional_one_time_payment: {
+          amount: 678,
+          metadata: 'order-5678',
         },
+      } as const;
+
+      const client = new Client(key);
+      const subscription = await client.initiateSubscription(data);
+
+      expect(subscription).toBe(location);
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/subscription',
+        {},
+        serialise({ ...data, token }),
       );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
+    });
+
+    it('fails if the response from the server is not a redirect', async () => {
+      const response = new Response('all good?', { status: 200 });
+      makePostRequestMock.mockResolvedValueOnce(response);
+
+      const client = new Client(key);
+
+      await expect(
+        client.initiateSubscription(defaults),
+      ).rejects.toBeInstanceOf(PPError);
+      expect(makePostRequestMock).toHaveBeenCalledWith(
+        '/api/integration/generic/subscription',
+        {},
+        serialise({ ...defaults, token }),
+      );
+      expect(makePutRequestMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('updatePaymentMethod', () => {
+    it('updates a payment method', async () => {
+      const response = new Response(undefined, { status: 204 });
+      makePutRequestMock.mockResolvedValueOnce(response);
+
+      const data = { type: PaymentMethodType.Card, amount: 234 };
+
+      const client = new Client(key);
+      await client.updatePaymentMethod('cb35f971', { ...data, urn, email });
+
+      expect(makePutRequestMock).toHaveBeenCalledWith(
+        '/api/payment-method/cb35f971',
+        { Authorization: authorization },
+        serialise(data),
+      );
+      expect(makePostRequestMock).not.toHaveBeenCalled();
+    });
+
+    it('updates a payment method with a voucher provider', async () => {
+      const response = new Response(undefined, { status: 204 });
+      makePutRequestMock.mockResolvedValueOnce(response);
+
+      const data = {
+        type: PaymentMethodType.ChildcareVouchers,
+        amount: 234,
+        voucher_provider: 'fun_for_kids',
+      };
+
+      const client = new Client(key);
+      await client.updatePaymentMethod('cb35f971', { ...data, urn, email });
+
+      expect(makePutRequestMock).toHaveBeenCalledWith(
+        '/api/payment-method/cb35f971',
+        { Authorization: authorization },
+        serialise(data),
+      );
+      expect(makePostRequestMock).not.toHaveBeenCalled();
+    });
+
+    it('fails if the response from the server is not satisfactory', async () => {
+      const response = new Response('all good?', { status: 200 });
+      makePutRequestMock.mockResolvedValueOnce(response);
+
+      const data = { type: PaymentMethodType.Card, amount: 234 };
+
+      const client = new Client(key);
+      await expect(
+        client.updatePaymentMethod('cb35f971', { ...data, urn, email }),
+      ).rejects.toBeInstanceOf(PPError);
+
+      expect(makePutRequestMock).toHaveBeenCalledWith(
+        '/api/payment-method/cb35f971',
+        { Authorization: authorization },
+        serialise(data),
+      );
+      expect(makePostRequestMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('refundPaymentMethod', () => {
+    it('refunds a payment method', async () => {
+      const response = new Response(undefined, { status: 204 });
+      makePutRequestMock.mockResolvedValueOnce(response);
+
+      const data = { amount: 234 };
+
+      const client = new Client(key);
+      await client.refundPaymentMethod('cb35f971', { ...data, urn, email });
+
+      expect(makePutRequestMock).toHaveBeenCalledWith(
+        '/api/payment-method/cb35f971/refund',
+        { Authorization: authorization },
+        serialise(data),
+      );
+      expect(makePostRequestMock).not.toHaveBeenCalled();
+    });
+
+    it('fails if the response from the server is not satisfactory', async () => {
+      const response = new Response('all good?', { status: 200 });
+      makePutRequestMock.mockResolvedValueOnce(response);
+
+      const data = { amount: 234 };
+
+      const client = new Client(key);
+      await expect(
+        client.refundPaymentMethod('cb35f971', { ...data, urn, email }),
+      ).rejects.toBeInstanceOf(PPError);
+
+      expect(makePutRequestMock).toHaveBeenCalledWith(
+        '/api/payment-method/cb35f971/refund',
+        { Authorization: authorization },
+        serialise(data),
+      );
+      expect(makePostRequestMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('cancelPayment', () => {
+    it('cancels a payment', async () => {
+      const response = new Response(undefined, { status: 204 });
+      makePutRequestMock.mockResolvedValueOnce(response);
+
+      const client = new Client(key);
+      await client.cancelPayment('d34e567a', { urn, email });
+
+      expect(makePutRequestMock).toHaveBeenCalledWith(
+        '/api/payment/d34e567a/cancel',
+        { Authorization: authorization },
+        serialise({}),
+      );
+      expect(makePostRequestMock).not.toHaveBeenCalled();
+    });
+
+    it('fails if the response from the server is not satisfactory', async () => {
+      const response = new Response('all good?', { status: 200 });
+      makePutRequestMock.mockResolvedValueOnce(response);
+
+      const client = new Client(key);
+      await expect(
+        client.cancelPayment('d34e567a', { urn, email }),
+      ).rejects.toBeInstanceOf(PPError);
+
+      expect(makePutRequestMock).toHaveBeenCalledWith(
+        '/api/payment/d34e567a/cancel',
+        { Authorization: authorization },
+        serialise({}),
+      );
+      expect(makePostRequestMock).not.toHaveBeenCalled();
     });
   });
 });
